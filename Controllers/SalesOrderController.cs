6c29e76529ae98776fe56949e074d97c57e8aed4
meythabethany <meythabethany@gmail.com>
2024-12-22 16:35:17 +0700

    Model Sales Order - Untuk List Home View SalesOrder Controller Sales Order - (GetSalesOrders)

---
+++ Controllers/SalesOrderController.cs
@@ -0,0 +1,67 @@
+﻿using Meytha_ProfesTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace Meytha_ProfesTest.Controllers
+{
+    public class SalesOrderController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public SalesOrderController(IConfiguration ConnectionStrings)
+        {
+            _configuration = ConnectionStrings;
+        }
+
+        public IActionResult Index(string keyword, DateTime? orderDate)
+        {
+            var salesOrders = GetSalesOrders();
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                salesOrders = salesOrders.FindAll(x =>
+                    x.SalesOrderNumber.Contains(keyword, StringComparison.OrdinalIgnoreCase) ||
+                    x.Customer.Contains(keyword, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (orderDate.HasValue)
+            {
+                salesOrders = salesOrders.FindAll(x => x.OrderDate.Date == orderDate.Value.Date);
+            }
+
+            return View(salesOrders);
+        }
+
+        private List<SalesOrder> GetSalesOrders()
+        {
+            var salesOrders = new List<SalesOrder>();
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("usp_sales_order_lists", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            salesOrders.Add(new SalesOrder
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                SalesOrderNumber = reader["SalesOrderNumber"].ToString(),
+                                OrderDate = Convert.ToDateTime(reader["OrderDate"]),
+                                Customer = reader["Customer"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+
+            return salesOrders;
+        }
+
+    }
+}
---
+++ Models/SalesOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace Meytha_ProfesTest.Models
+{
+    public class SalesOrder
+    {
+        public int Id { get; set; }
+        public string SalesOrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Customer { get; set; }
+    }
+}
